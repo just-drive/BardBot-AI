129993f2214fc216c66c0d1314933c560eae07b9
RichGitCoding <richard.robertsoncareers@gmail.com>
2022-04-14 00:26:55 -0500

    Added components folders and about me page section

---
+++ web-app/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import  { AiFillGithub } from 'react-icons/ai';
+import { DiCssdeck } from 'react-icons/di';
+
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
+
+const Header = () => (
+    <div>
+        Header
+    </div>
+);
+
+export default Header;
