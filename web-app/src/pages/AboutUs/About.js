06277dcea421aaff362ff8633e3706473f4852fe
RichGitCoding <richard.robertsoncareers@gmail.com>
2022-05-01 00:29:10 -0500

    Navbar. Added react-icons, styled-components, and react-router-dom

---
+++ web-app/src/pages/AboutUs/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+
+
+
+
+const About = () => {
+  return (
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center",height: '90vh'}}>
+      About
+    </div>
+  );
+};
+
+export default About;
