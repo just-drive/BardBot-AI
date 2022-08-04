564b8dea7a12814e4e93fc3bc8bb2a95b5de1c45
RichGitCoding <richard.robertsoncareers@gmail.com>
2022-08-04 16:52:23 -0500

    All updated version

--- web-app/src/components/LyricGenerator/LyricGenerator.js
+++ web-app/src/components/LyricGenerator/LyricGenerator.js
@@ -58,35 +58,52 @@ class LyricGenerator extends React.Component {
   handleSubmit = async (event) => {
     event.preventDefault();
     const prompt =
-      (this.state.title.trim().length != 0 ? `Title: \n${this.state.title}\n` : '') +
-      (this.state.genre.trim().length != 0 ? `Genre: ${this.state.genre}\n` : '') +
-      (this.state.theme.trim().length != 0 ? `Theme: ${this.state.theme}\n` : '') +
+      (this.state.title.trim().length != 0
+        ? `Title: \n${this.state.title}\n`
+        : "") +
+      (this.state.genre.trim().length != 0
+        ? `Genre: ${this.state.genre}\n`
+        : "") +
+      (this.state.theme.trim().length != 0
+        ? `Theme: ${this.state.theme}\n`
+        : "") +
       `Lyrics:\n`;
-    const completion = await query({prompt: prompt});
+    const completion = await query({ prompt: prompt });
     console.log(completion);
     alert(completion);
-  
-  }
+  };
 
   render() {
     console.log(this.state);
     return (
       <div>
         <div>
-          <input id="genre_input" onChange={this.handleGenreChange} 
-            placeholder="Genre" />
+          <input
+            id="genre_input"
+            onChange={this.handleGenreChange}
+            placeholder="Genre"
+          />
         </div>
         <div>
-          <input id="pace_input" onChange={this.handlePaceChange} 
-            placeholder="Pace (bpm)" />
+          <input
+            id="pace_input"
+            onChange={this.handlePaceChange}
+            placeholder="Pace (bpm)"
+          />
         </div>
         <div>
-          <input id="theme_input" onChange={this.handleThemeChange}
-            placeholder="Theme" />
+          <input
+            id="theme_input"
+            onChange={this.handleThemeChange}
+            placeholder="Theme"
+          />
         </div>
         <div>
-          <input id="title_input" onChange={this.handleTitleChange}
-           placeholder="Title" />
+          <input
+            id="title_input"
+            onChange={this.handleTitleChange}
+            placeholder="Title"
+          />
         </div>
         <button type="submit" value="Submit" onClick={this.handleSubmit}>
           Submit
