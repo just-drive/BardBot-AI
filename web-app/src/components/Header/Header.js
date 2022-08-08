41dffbad7fac698bdf07a3894a606d705e59fb40
RichGitCoding <richard.robertsoncareers@gmail.com>
2022-08-08 15:59:42 -0500

    Responsive Navbar

--- web-app/src/components/Header/Header.js
+++ web-app/src/components/Header/Header.js
@@ -9,40 +9,88 @@ import Toggle from "../ThemeToggle/Toggle";
 import { useDarkMode } from "components/ThemeToggle/userDarkMode";
 import { GlobalStyles } from "pages/App/global";
 import { darkTheme, lightTheme } from "components/ThemeToggle/themes";
+import {
+  AppBar,
+  makeStyles,
+  Typography,
+  Toolbar,
+  InputBase,
+} from "@material-ui/core";
+const useStyles = makeStyles((Theme) => ({
+  appbar: {
+    backgroundColor: "transparent",
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-evenly",
+    background: "#000",
+    color: "#000",
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+    borderStyle: "solid",
+    borderRadius: "20px",
+    borderWidth: "2px",
+    borderColor: "#ff3d00",
+  },
+  logoLg: {
+    display: "none",
+    [Theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  logoSm: {
+    display: "block",
+    [Theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
   const [theme, toggleTheme, componentMounted] = useDarkMode();
   const themeMode = theme === "light" ? lightTheme : darkTheme;
   if (!componentMounted) {
     return <div></div>;
   }
+
   return (
     <>
-      <Nav>
-        <div class="left">
-          <NavLink to="/">
-            <img src={bbai} alt="Logo" exact />
-          </NavLink>
-        </div>
-        <Bars />
-        <NavMenu>
-          <NavLink to="/about" activestyle>
-            .about()
-          </NavLink>
-          <NavLink to="/" activestyle>
-            .contactUs()
-          </NavLink>
-          <a href="https://github.com/just-drive/BardBot-AI" activestyle>
-            .sourceCode()
-          </a>
-          <div class="right">
+      <AppBar className={classes.appbar}>
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h6" className={classes.logoLg}>
+            <div class="left">
+              <NavLink to="/">
+                <img src={bbai} alt="Logo" exact />
+              </NavLink>
+            </div>
+          </Typography>
+          <Typography variant="h6">
+            <NavLink to="/about" activestyle>
+              .about()
+            </NavLink>
+          </Typography>
+          <Typography variant="h6">
+            <NavLink to="/" activestyle>
+              .contactUs()
+            </NavLink>
+          </Typography>
+          <Typography variant="h6">
+            <a href="https://github.com/just-drive/BardBot-AI" activestyle>
+              .sourceCode()
+            </a>
+          </Typography>
+          <Typography variant="h6">
+            {/* <div class="right"> */}
             <ThemeProvider theme={themeMode}>
               <Toggle theme={theme} toggleTheme={toggleTheme} />
               <GlobalStyles />
             </ThemeProvider>
-          </div>
-        </NavMenu>
-      </Nav>
+          </Typography>
+          {/* <Typography variant="h6" className={classes.logoSm}>
+            Not Hello
+          </Typography> */}
+        </Toolbar>
+      </AppBar>
     </>
   );
 };
